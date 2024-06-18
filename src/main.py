1a535dcebf34386a3c21720286fa8c1741ae3cfc
soliprem <franci.solidoro@gmail.com>
2024-06-18 19:10:29 +0200

    recursive generation

--- src/main.py
+++ src/main.py
@@ -41,9 +41,23 @@ def generate_page(from_path, template_path, dest_path):
         f.write(html)
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    os.mkdir(dest_dir_path) if not os.path.exists(
+        dest_dir_path) else print(f"{dest_dir_path} exists")
+    listdir = os.listdir(dir_path_content)
+    for item in listdir:
+        if os.path.isfile(os.path.join(dir_path_content, item)):
+            generate_page(os.path.join(dir_path_content, item), template_path,
+                          os.path.join(
+                dest_dir_path, item.replace(".md", ".html")))
+        else:
+            generate_pages_recursive(os.path.join(dir_path_content, item), "template.html",
+                                     os.path.join(dest_dir_path, item))
+
+
 def main():
     replacetree('static', 'public')
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content/", "template.html", "public/")
 
 
 main()
